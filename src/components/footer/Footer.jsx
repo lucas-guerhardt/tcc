016216ae3dc903528e820ae80fd942491c461139
pinheiroo <lucaspin03@hotmail.com>
2024-07-01 17:57:14 -0300

    Estrutura do front finalizada

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,12 +1,17 @@
-import styles from './footer.module.css';
+import Link from "next/link";
+import styles from "./footer.module.css";
 
 const Footer = () => {
-    return(
-        <div className={styles.container}>
-            <div className={styles.logo}>pindev</div>
-            <div className={styles.text}>Desenvolvido com ♥ por Lucas Guerhardt Pinheiro. Agradecimentos à <a href="https://www.youtube.com/@LamaDev" target="_blank" rel="noopener noreferrer">LamaDev</a>© All rights reserved.</div>
-        </div>
-    );
-}
+  return (
+    <div className={styles.container}>
+      <div className={styles.logo}>pindev</div>
+      <div className={styles.text}>
+        Desenvolvido com ♥ por Lucas Guerhardt Pinheiro. Agradecimentos à
+        <Link href="https://www.youtube.com/@LamaDev">{" LamaDev"}</Link>© All
+        rights reserved.
+      </div>
+    </div>
+  );
+};
 
 export default Footer;
