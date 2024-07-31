0a456bbf13d1d86229f23fa12c049c35639a00e7
pinheiroo <lucaspin03@hotmail.com>
2024-07-31 18:44:03 -0300

    adicionando gh

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,7 +1,7 @@
 "use client";
 
 import Image from "next/image";
-import { useRouter }from "next/navigation";
+import { useRouter } from "next/navigation";
 import styles from "./homepage.module.css";
 
 const Home = () => {
@@ -12,23 +12,61 @@ const Home = () => {
       <div className={styles.txtContainer}>
         <h1 className={styles.title}>Você está a um sopro da clareza.</h1>
         <p className={styles.desc}>
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veritatis harum, aliquid laboriosam explicabo dolor odit soluta?
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veritatis
+          harum, aliquid laboriosam explicabo dolor odit soluta?
         </p>
         <div className={styles.buttons}>
-          <button className={styles.button} onClick={() => router.push("/practice")}>Comece Já</button>
-          <button className={styles.button} onClick={() => router.push("/about")}>Saiba Mais</button>
+          <button
+            className={styles.button}
+            onClick={() => router.push("/practice")}
+          >
+            Comece Já
+          </button>
+          <button
+            className={styles.button}
+            onClick={() => router.push("/about")}
+          >
+            Saiba Mais
+          </button>
         </div>
         <div className={styles.brands}>
-          <a href="https://www.linkedin.com/in/lucas-guerhardt-7a558a237/" target="_blank" rel="noopener noreferrer">
-            <Image src="/linkedin.png" alt="LinkedIn" width={25} height={25} className={styles.inImg}/>
+          <a
+            href="https://www.linkedin.com/in/lucas-guerhardt-7a558a237/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Image
+              src="/linkedin.png"
+              alt="LinkedIn"
+              width={25}
+              height={25}
+              className={styles.inImg}
+            />
           </a>
-          <a href="https://github.com/pinheiroo" target="_blank" rel="noopener noreferrer">
-            <Image src="/github.png" alt="GitHub" width={25} height={25} className={styles.ghImg}/>
+          <a
+            href="https://github.com/lucas-guerhardt"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Image
+              src="/github.png"
+              alt="GitHub"
+              width={25}
+              height={25}
+              className={styles.ghImg}
+            />
           </a>
         </div>
       </div>
       <div className={styles.imgContainer}>
-        <Image src="/hero.gif" alt="" fill className={styles.heroImg} sizes="(max-width: 1280px)" priority={false}/>
+        <Image
+          src="/hero.gif"
+          alt=""
+          fill
+          className={styles.heroImg}
+          sizes="(max-width: 1280px)"
+          priority={false}
+        />
       </div>
     </div>
   );
