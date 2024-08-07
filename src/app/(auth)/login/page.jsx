dda3f2f539be416538121c97444eb1f587ebf919
Lucas Pinheiro <lucaspin03@hotmail.com>
2024-08-07 10:57:14 -0300

    tentando fazer tela login

--- src/app/(auth)/login/page.jsx
+++ src/app/(auth)/login/page.jsx
@@ -1,11 +1,26 @@
 import { handleGoogleLogin } from "@/lib/action";
-
+import styles from "./login.module.css";
+import Image from "next/image";
 const LoginPage = async () => {
   return (
-    <div>
-      <form action={handleGoogleLogin}>
-        <button>Login With Google</button>
-      </form>
+    <div className={styles.container}>
+      <div className={styles.imgContainer}>
+        <Image
+          src="/contact.png"
+          alt=""
+          fill
+          className={styles.img}
+          sizes="(max-width: 540px)"
+        />
+      </div>
+      <div className={styles.txtContainer}>
+        <h1 className={styles.title}>Inicie Fazendo o Login</h1>
+        <div className={styles.connectBtn}>
+          <form action={handleGoogleLogin}>
+            <button>Login With Google</button>
+          </form>
+        </div>
+      </div>
     </div>
   );
 };
