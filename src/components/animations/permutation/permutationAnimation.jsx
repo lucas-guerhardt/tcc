ba4c0ee04b87ed868745f673c220124153560980
pinheiroo <lucaspin03@hotmail.com>
2024-09-30 11:52:11 -0300

    bora bill

--- src/components/animations/permutation/permutationAnimation.jsx
+++ src/components/animations/permutation/permutationAnimation.jsx
@@ -6,13 +6,22 @@ import Permuter from "./permuter/permuter";
 
 const PermutationAnimation = () => {
   const [currentPage, setCurrentPage] = useState(0);
+  const [isAnimating, setIsAnimating] = useState(false); // Controlar a animação
 
   const nextPage = () => {
-    if (currentPage < pages.length - 1) {
-      setCurrentPage((prevPage) => prevPage + 1);
-    } else {
-      setCurrentPage(0);
-    }
+    // Adiciona animação de escala quando mudar de página
+    setIsAnimating(true);
+
+    setTimeout(() => {
+      setIsAnimating(false);
+
+      // Atualiza a página depois da animação
+      if (currentPage < pages.length - 1) {
+        setCurrentPage((prevPage) => prevPage + 1);
+      } else {
+        setCurrentPage(0);
+      }
+    }, 300); // Duração da animação de 300ms
   };
 
   const goToPage = (pageIndex) => {
@@ -21,14 +30,22 @@ const PermutationAnimation = () => {
 
   return (
     <div className={styles.container}>
-      <div className={styles.explanation}>
-        <div className={styles.explanationContainer}>
+      <div
+        className={`${styles.explanationContainer} ${
+          isAnimating ? styles["scale-up"] : ""
+        }`}
+      >
+        <div className={styles.explanation}>
           {pages[currentPage].content}
           <button className={styles.nextBtn} onClick={nextPage}>
             Próximo
           </button>
         </div>
-        <Permuter />
+
+        <div className={styles.postIt}>
+          <Permuter />
+        </div>
+
         <div className={styles.pageIndicators}>
           {pages.map((_, index) => (
             <div
